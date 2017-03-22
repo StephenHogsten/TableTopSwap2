c90bfdcf5135c3b6436f7688c01596590320be84
HogDogTheGod <throweymcthrowerson@gmail.com>
2017-03-21 23:57:22 -0400

    the basic-est of react-routes

--- src/clientJS/App.js
+++ src/clientJS/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import logo from '../logo.svg';
 import '../scss/App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router> 
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <div>
+            <Link to='/about'>Take me to About</Link><br />
+            <Link to='/'>Take me to Home</Link>
+            <Route path="/about" component={About}/>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
+
+const About = ({ match }) => (
+  <div className="HogDog">
+    MEMEMEME
+  </div>
+)
 
 export default App;
